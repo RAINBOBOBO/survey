cad33b484a6e4b03525fdcf2d4bde81fc9cba868
Rain Babauta <rainbabauta1@gmail.com>
2020-07-23 14:59:06 -0700

    setup html templates and wrote first route function

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template, redirect, jsonify, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "very-secret"
+
+# debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.route("/")
+def show_survey():
+  """renders a page to show a survey and a button"""
+  return render_template("survey.html", title_in_template=satisfaction_survey.title, instructions_in_template=satisfaction_survey.instructions)
