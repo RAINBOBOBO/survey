2b524bd51775603d0f46cd65e2cb3646ae9c0446
Wayne Chen <wayne12190@gmail.com>
2020-07-23 16:14:33 -0700

    implemented /question route and updated corresponding html template

--- app.py
+++ app.py
@@ -1,19 +1,54 @@
-<<<<<<< HEAD
 from flask import Flask, request, render_template, redirect, jsonify, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import satisfaction_survey
+# from surveys import satisfaction_survey #TODO: uncomment later
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "very-secret"
 
-# debug = DebugToolbarExtension(app)
+debug = DebugToolbarExtension(app)
 
 responses = []
+
+# for reference only TODO: delete later
+class Question:
+    def __init__(self, question, choices=None, allow_text=False):
+        if not choices:
+            choices = ["Yes", "No"]
+
+        self.question = question
+        self.choices = choices
+        self.allow_text = allow_text
+
+
+class Survey:
+    def __init__(self, title, instructions, questions):
+        self.title = title
+        self.instructions = instructions
+        self.questions = questions
+
+satisfaction_survey = Survey(
+    "Customer Satisfaction Survey",
+    "Please fill out a survey about your experience with us.",
+    [
+        Question("Have you shopped here before?"),
+        Question("Did someone else shop with you today?"),
+        Question("On average, how much do you spend a month on frisbees?",
+                 ["Less than $10,000", "$10,000 or more"]),
+        Question("Are you likely to shop here again?"),
+    ])
 
 @app.route("/")
 def show_survey():
   """renders a page to show a survey and a button"""
   return render_template("survey.html", title_in_template=satisfaction_survey.title, instructions_in_template=satisfaction_survey.instructions)
-=======
-# testing
->>>>>>> refs/remotes/origin/master
+
+@app.route("/questions/<str_question>")
+def show_questions(str_question):
+  """shows questions"""
+  num_question = int(str_question)
+
+  return render_template("questions.html",
+    question_in_template = satisfaction_survey.questions[num_question],
+    num_question_in_template = num_question,
+    choices_in_template = satisfaction_survey.questions[num_question].choices,
+    num_choices_in_template = len(satisfaction_survey.questions[num_question].choices))
